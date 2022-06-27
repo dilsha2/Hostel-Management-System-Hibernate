03cb747bd24f6f38337c804890f2e0f4d6e9f8a3
dilsha2 <diliprasanna96@gmail.com>
2022-06-27 19:21:22 +0530

    Create Login Form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane loginContext;
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
 }
